f66d23cc75c97213660209e44d22d60b837692b0
Oleg Dats <tezavrat@gmail.com>
2014-02-21 23:37:28 +0200

    Store config in local storage

--- www/js/app/app.js
+++ www/js/app/app.js
@@ -20,18 +20,7 @@ phonecatApp.config(['$routeProvider',
       }).
       otherwise({
         redirectTo: function() {
-        	var applaunchCount = window.localStorage.getItem('launchCount');
-            //Check if it already exists or not
-            if(applaunchCount){
-               //This is a second time launch, and count = applaunchCount
-               console.log("NO, it's not a first time");
-               return '/taxi'
-            }else{
-              //Local storage is not set, hence first time launch. set the local storage item
-              window.localStorage.setItem('launchCount',1);
-              //Do the other stuff related to first time launch
-              return '/init'
-            }
+        	return '/taxi';
         }
       });
   }]);
--- www/js/app/controllers.js
+++ www/js/app/controllers.js
@@ -6,14 +6,25 @@ var appControllers = angular.module('appControllers', []);
 
 appControllers.controller('TaxiListCtrl', ['$scope', 'datasource',
   function($scope, datasource) {
-    $scope.phones = datasource.getTaxi();
+    if(!datasource.hasConfig()) {
+      window.location.assign("#/init");
+    } else {
+      $scope.phones = datasource.getTaxi();
+    }
   }]);
 
 appControllers.controller('ConfigCtrl', ['$scope', 'datasource',
   function($scope, datasource) {
     var myConfig = datasource.getMyConfig();
-    $scope.city = myConfig.city;
-    $scope.operator = myConfig.operator;
+    $scope.cities = datasource.getCities();
+    $scope.operators = datasource.getOperators();
+    $scope.myCity = myConfig.city;
+    $scope.myOperator = myConfig.operator;
+
+    $scope.saveConfig = function() {
+      datasource.saveMyConfig($scope.myCity, $scope.myOperator);
+      window.location.assign("#/taxi");
+    }
   }]);
 
 
@@ -21,10 +32,50 @@ appControllers.controller('ConfigCtrl', ['$scope', 'datasource',
 
 angular.module('appControllers').service("datasource", function() {
     this.getMyConfig = function() {
-      return {
-        city: 'lviv',
-        operator: 'Life'
+      if(this.hasConfig()) {
+        return this.get('myConfig');;
+      } else {
+        console.log('error, empty config');
+        return {
+          city: 'lviv',
+          operator: 'ks'
+        }
       }
+    },
+
+    this.saveMyConfig = function(city, operator) {
+      var myConfig = {
+        city: city,
+        operator: operator
+      }
+      console.log('save', myConfig)
+      this.save('myConfig', myConfig);
+    },
+
+    this.hasConfig = function() {
+      var myConfig = this.get('myConfig');
+      if(myConfig && myConfig.city && myConfig.operator) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+
+    this.getCities = function() {
+      var cities = [
+        {'title': 'Львів', 'key': 'lviv'},
+        {'title': 'Київ', 'key': 'kiev'}
+      ]
+      return cities;
+    },
+
+    this.getOperators = function() {
+      var operators = [
+        {'title': 'Київстар', 'key': 'ks'},
+        {'title': 'МТС', 'key': 'mts'},
+        {'title': 'life:)', 'key': 'life'},       
+      ]
+      return operators;
     },
 
     this.getTaxi = function() {
@@ -43,5 +94,21 @@ angular.module('appControllers').service("datasource", function() {
          'phone': '0631505624'}
       ]
       return result;
+    },
+
+    this.save = function(key, _object) {
+      window.localStorage.setItem(key, JSON.stringify(_object));
+    },
+
+    this.get = function(key) {
+      return JSON.parse(window.localStorage.getItem(key));
     }
+
   });
+
+
+/* Event handlers */
+
+$(document).on( "click", ".save-config1", function() {
+  alert( $( this ).text() );
+});
