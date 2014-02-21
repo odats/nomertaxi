75f9635dddcd9b058aa44acd0ba801c822c0fcb4
Oleg Dats <tezavrat@gmail.com>
2014-02-21 22:02:36 +0200

    Improve list of taxi

--- www/js/app/controllers.js
+++ www/js/app/controllers.js
@@ -7,19 +7,19 @@ var phonecatControllers = angular.module('phonecatControllers', []);
 phonecatControllers.controller('PhoneListCtrl', ['$scope',
   function($scope) {
     $scope.phones = [
-		{'name': 'Наше таксі плюс', 'avatar': 'mock/images/1.png',
-		 'phone': '0631505624'},
-		{'name': 'Оптимальне', 'avatar': 'mock/images/2.png',
-		 'phone': '0631505624'},
-		{'name': 'Браво', 'avatar': 'mock/images/3.png',
-		 'phone': '0631505624'},
-		 {'name': 'Наше таксі плюс', 'avatar': 'mock/images/1.png',
-		 'phone': '0631505624'},
-		{'name': 'Оптимальне', 'avatar': 'mock/images/2.png',
-		 'phone': '0631505624'},
-		{'name': 'Браво', 'avatar': 'mock/images/3.png',
-		 'phone': '0631505624'},
-  	];
+      {'name': 'Наше таксі плюс', 'avatar': 'mock/images/1.png',
+       'phone': '0631505624'},
+      {'name': 'Оптимальне', 'avatar': 'mock/images/2.png',
+       'phone': '0631505624'},
+      {'name': 'Браво', 'avatar': 'mock/images/3.png',
+       'phone': '0631505624'},
+       {'name': 'Наше таксі плюс', 'avatar': 'mock/images/1.png',
+       'phone': '0631505624'},
+      {'name': 'Оптимальне', 'avatar': 'mock/images/2.png',
+       'phone': '0631505624'},
+      {'name': 'Браво', 'avatar': 'mock/images/3.png',
+       'phone': '0631505624'},
+    ];
   }]);
 
 phonecatControllers.controller('InitCtrl', ['$scope',
